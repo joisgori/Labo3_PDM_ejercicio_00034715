57cd9c04ea5c3a4a1e3f1e452ff3803bc19ba367
joisgori <00034715@uca.edu.sv>
2019-03-23 10:33:46 -0600

    Ya funciona el botón share, pero solo comparte el texto de género

--- app/src/main/java/com/example/formulario/Compartido.java
+++ app/src/main/java/com/example/formulario/Compartido.java
@@ -1,13 +1,30 @@
 package com.example.formulario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Compartido extends AppCompatActivity {
+
+    private static TextView sUser, sPass, sEmail, sGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compartido);
+
+        sUser = findViewById(R.id.User_Com);
+        sPass = findViewById(R.id.Pass_Com);
+        sEmail = findViewById(R.id.Email_Com);
+        sGender = findViewById(R.id.Gender_Com);
+
+        Intent ulIntent = getIntent();
+
+        if (ulIntent!=null){
+
+            sUser.setText(ulIntent.getStringExtra(ulIntent.EXTRA_TEXT)); //Esta debe ser la misma llave
+
+        }
     }
 }
--- app/src/main/java/com/example/formulario/Datos.java
+++ app/src/main/java/com/example/formulario/Datos.java
@@ -3,6 +3,7 @@ package com.example.formulario;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 //import static com.example.formulario.AppConstant.TEXT_CONTRASEÑA;
@@ -16,6 +17,7 @@ import static com.example.formulario.AppConstant.TEXT_USUARIO;
 public class Datos extends AppCompatActivity {
 
     private static TextView txt_UsuarioRecibido, txt_ContraRecibida, txt_GeneroRecibido, txt_EmailRecibido;
+    private static Button mBotonCompartir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,8 @@ public class Datos extends AppCompatActivity {
         txt_EmailRecibido = findViewById(R.id.txt_email);
         txt_GeneroRecibido = findViewById(R.id.txt_genero);
 
+        //Busco mi botón share por id
+        mBotonCompartir = findViewById(R.id.btn_compartir);
 
         Intent nIntent = getIntent();
 
@@ -37,5 +41,45 @@ public class Datos extends AppCompatActivity {
             txt_GeneroRecibido.setText(nIntent.getStringExtra(TEXT_GENERO));
 
         }
+
+        //Acá creo mi evento para mandarlo a la nueva activity
+        mBotonCompartir.setOnClickListener(v -> {
+            //Acá reuso las variables que ya tienen el contenido
+            String textU = txt_UsuarioRecibido.getText().toString();
+            String textP = txt_EmailRecibido.getText().toString();
+            String textE = txt_ContraRecibida.getText().toString();
+            String textG = txt_GeneroRecibido.getText().toString();
+
+            Intent UserIntent = new Intent();
+            /*Intent PassIntent = new Intent();
+            Intent EmailIntent = new Intent();
+            Intent GeneroIntent = new Intent();*/
+
+            //Esto lo setteamos para decirle a la aplicación qué tipo de datos va a recibir y está lista...
+
+            UserIntent.setType("text/plain");
+            /*PassIntent.setType("text/plain");
+            EmailIntent.setType("text/plain");
+            GeneroIntent.setType("text/plain");*/
+
+            //Setteamos un tipo de acción
+
+            UserIntent.setAction(Intent.ACTION_SEND);
+            /*PassIntent.setAction(Intent.ACTION_SEND);
+            EmailIntent.setAction(Intent.ACTION_SEND);
+            GeneroIntent.setAction(Intent.ACTION_SEND);*/
+
+            //Acá hay una llave que ya la co
+
+            UserIntent.putExtra(Intent.EXTRA_TEXT, textU);
+            /*PassIntent.putExtra(Intent.EXTRA_TEXT, textP);
+            EmailIntent.putExtra(Intent.EXTRA_TEXT, textE);
+            GeneroIntent.putExtra(Intent.EXTRA_TEXT, textG);*/
+
+            startActivity(UserIntent);
+            /*startActivity(PassIntent);
+            startActivity(EmailIntent);
+            startActivity(GeneroIntent);*/
+        });
     }
 }
